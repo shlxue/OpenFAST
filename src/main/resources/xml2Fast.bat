889c10c7a68a558f86da73378a64fb0898a29bc8
jacob_northey <jacob_northey@e6ee02d6-d133-0410-bd08-f9337e8129a4>
2008-10-16 18:58:49 +0000

    Fixed batch scripts
    git-svn-id: svn://svn.code.sf.net/p/openfast/code/trunk@191 e6ee02d6-d133-0410-bd08-f9337e8129a4

--- src/main/resources/xml2Fast.bat
+++ src/main/resources/xml2Fast.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set OPENFAST_HOME=%~dp0%\..
+set OPENFAST_HOME=%~dp0%..
 if not "%JAVA_HOME%" == "" goto GotJava
 echo Please set the JAVA_HOME environment variable to the install location of a valid JRE.
 exit /B 1
