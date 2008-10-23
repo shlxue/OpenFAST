1add526a3444a1dde20935069d4d1a8f1f1211b4
jacob_northey <jacob_northey@e6ee02d6-d133-0410-bd08-f9337e8129a4>
2008-10-23 16:47:27 +0000

    Re-instated 1.4 compatibility by removing reference to 1.5 interface Iterable
    git-svn-id: svn://svn.code.sf.net/p/openfast/code/trunk@193 e6ee02d6-d133-0410-bd08-f9337e8129a4

--- src/main/java/org/openfast/template/TemplateRegistry.java
+++ src/main/java/org/openfast/template/TemplateRegistry.java
@@ -24,7 +24,7 @@ import java.util.Iterator;
 
 import org.openfast.QName;
 
-public interface TemplateRegistry extends Iterable {
+public interface TemplateRegistry {
     TemplateRegistry NULL = new NullTemplateRegistry();
 
     void registerAll(TemplateRegistry registry);
