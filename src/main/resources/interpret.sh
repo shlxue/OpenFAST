55dd08fcb0c181ce8e760c096413dd177a93a224
jacob_northey <jacob_northey@e6ee02d6-d133-0410-bd08-f9337e8129a4>
2008-12-12 22:32:22 +0000

    Added interpret example submitted by Dale Wilson from OCI
    git-svn-id: svn://svn.code.sf.net/p/openfast/code/trunk@204 e6ee02d6-d133-0410-bd08-f9337e8129a4

---
+++ src/main/resources/interpret.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! $JAVA_HOME ]; then
+echo "Please set the JAVA_HOME environment variable to point to a valid Java install location."
+exit 1
+fi
+
+if [ ! -f $JAVA_HOME/bin/java ]; then
+echo "Please set the JAVA_HOME environment variable to point to a valid Java install location."
+exit 1
+fi
+
+OPENFAST_HOME=$( cd -P -- "$(dirname -- "$0")" && pwd -P)/..
+JAVA="$JAVA_HOME/bin/java"
+
+CLASSPATH=$OPENFAST/lib/commons-cli-1.1.jar:$OPENFAST/${project.artifactId}-${project.version}.jar
+MAIN=org.openfast.examples.interpret.Main
+$JAVA $JAVA_OPTS -classpath $CLASSPATH $MAIN $*
