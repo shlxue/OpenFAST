66ebec62c47dea142e69994f37e6cafdda830f98
ShlXue <XueShuiliang@gmail.com>
2016-04-15 10:38:51 +0800

    add test codes for some integer bound values

--- src/test/java/org/openfast/template/type/codec/UnsignedIntegerTest.java
+++ src/test/java/org/openfast/template/type/codec/UnsignedIntegerTest.java
@@ -1,6 +1,12 @@
 package org.openfast.template.type.codec;
 
+import org.openfast.template.LongValue;
 import org.openfast.test.OpenFastTestCase;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class UnsignedIntegerTest extends OpenFastTestCase {
 
@@ -18,4 +24,25 @@ public class UnsignedIntegerTest extends OpenFastTestCase {
         assertEncodeDecode(l(4294967295L), "00001111 01111111 01111111 01111111 11111111", TypeCodec.UINT);
     }
 
+    public void testSomeBoundValues() throws Exception {
+        List<Integer> intBounds = Arrays.asList(/* 128, */ 16384, 2097152, 268435456);
+        List<Long> longBounds = new ArrayList<Long>(Arrays.asList(34359738368L, 4398046511104L, 562949953421312L, 72057594037927936L));
+        for (int i : intBounds) {
+            longBounds.add((long) i);
+        }
+        List<Long> values = new ArrayList<Long>(longBounds.size() * 3);
+        for (long i : longBounds) {
+            values.add(i - 1);
+            values.add(i);
+            values.add(i + 1);
+        }
+        for (long i : values) {
+            byte[] bytes = TypeCodec.UINT.encode(new LongValue(i));
+            if (longBounds.indexOf(i) == -1) {
+                assertEquals(i, TypeCodec.UINT.decode(new ByteArrayInputStream(bytes)).toLong());
+            } else {
+                assertTrue(i != TypeCodec.UINT.decode(new ByteArrayInputStream(bytes)).toLong());
+            }
+        }
+    }
 }
